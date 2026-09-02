842934e6162a933d1fc3b6620cbe3d63eb1d0f92
Tejaswini Vaidya <tejurvaidya@gmail.com>
2026-09-02 13:30:18 +0530

    Add Product link to Navbar component

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -148,6 +148,13 @@ export default function Navbar() {
           >
             Case Studies
           </Link>
+           <Link
+            href="https://lacma.gigfactory.in"
+            className={`text-sm font-semibold transition-colors ${isActive('https://lacma.gigfactory.in') ? 'text-neon-green' : 'text-zinc-500 dark:text-zinc-300 hover:text-zinc-900 dark:hover:text-white'}`}
+          >
+            Product
+          </Link>
+          
           <div className="flex items-center justify-between p-3 mt-2">
             <span className="text-zinc-600 dark:text-gray-300 font-medium">Theme</span>
             <ThemeToggle />
